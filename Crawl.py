48851b27cab2d5dfd1b12b356f15c4debc7665c2
HoHuy <hohuy.work@gmail.com>
2023-10-05 22:23:30 +0700

    fixing something and add comment

--- Crawl.py
+++ Crawl.py
@@ -1,28 +1,28 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-# Request to website and download HTML contents
 
 def id_detection(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     rows = soup.find_all('main')
-
+    # Get id of movie
     for row in rows:
         id = row.find('a', {'class':'a-link-normal'}).get('href')
         id = id.split('/')
-        print(id)
         return id[4]
 
 def boxOfficeMojo(url):
+    # Request to website and download HTML contents
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
+    # Find all tag named 'tr'
     rows = soup.find_all('tr')
 
     for row in rows[1:]:
         id_url = 'https://www.boxofficemojo.com/' + row.find('a', {'class':'a-link-normal'}).get('href')
         id = id_detection(id_url)
-        
+        # Find necessary data
         rank = row.find('td',{'class':'mojo-header-column'}).text
         nameMovie = row.find('td',{'class':'mojo-field-type-release'}).text
         revenue = row.find('td',{'class':'mojo-field-type-money'}).text
@@ -31,6 +31,7 @@ def boxOfficeMojo(url):
         
         data = {"Rank": rank,"Name": nameMovie,"Revenue": revenue,"Gross Change": gross_change_by_date, "Studio": studio}
         final_data = json.dumps(data, indent=4)
-        print(final_data)
+        return final_data
+
 url = "https://www.boxofficemojo.com/date/2023-09-02/"
 boxOfficeMojo(url)
